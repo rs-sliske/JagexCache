7086dc000454ea8855256a444fd63575857c551d
Robert <Robert@Robert-Laptop>
2015-01-01 13:51:17 +0000

    tried to add stuff to dump quest data for my scripts

--- CacheViewer/src/FileSaver.java
+++ CacheViewer/src/FileSaver.java
@@ -5,26 +5,39 @@ public class FileSaver {
 
 	private PrintWriter file;
 
+	public FileSaver(String fileName, String path) {
+		createFile(path + fileName);
+	}
+
 	public FileSaver(String fileName) {
 		String directory = "C:\\users\\Robert";
 		directory += "\\models\\";
+		createFile(directory + fileName);
+	}
+
+	private final void createFile(String s) {
+
 		try {
-			file = new PrintWriter(directory + fileName);
+			file = new PrintWriter(s);
 
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 		if (file != null) {
-			System.out.println("file " + fileName + " created");
+			System.out.println("file " + s + " created");
 		}
-
 	}
 
 	public void append(String s) {
 		file.append(s);
 	}
 
+	public void appendln(String s){
+		append(s);
+		append("\n");
+	}
+	
 	public void close() {
 		file.close();
 
--- CacheViewer/src/Main.java
+++ CacheViewer/src/Main.java
@@ -1,8 +1,13 @@
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import com.sk.cache.DataSource;
 import com.sk.cache.fs.CacheSystem;
+import com.sk.cache.wrappers.ItemDefinition;
+import com.sk.cache.wrappers.QuestDefinition;
+import com.sk.cache.wrappers.loaders.ItemDefinitionLoader;
 import com.sk.cache.wrappers.loaders.NpcDefinitionLoader;
+import com.sk.cache.wrappers.loaders.QuestDefinitionLoader;
 
 public class Main {
 	public static final int SLISKE_ID = 14262;
@@ -20,12 +25,29 @@ public class Main {
 			DataSource ds = new DataSource(
 					DataSource.getDefaultCacheDirectory());
 			CacheSystem cache = new CacheSystem(ds);
-			NpcDefinitionLoader loader = new NpcDefinitionLoader(cache);
+			NpcDefinitionLoader npcLoader = new NpcDefinitionLoader(cache);
+			//
+			// new NPCSaver(loader.load(SLISKE_ID));
+			//
+			// saveModels(loader, VERAC_ID, GUTHAN_ID, TORAG_ID, DHAROK_ID,
+			// AHRIM_ID, AKRISAE_ID, KARIL_ID);
 
-			new NPCSaver(loader.load(SLISKE_ID));
-
-			saveModels(loader, VERAC_ID, GUTHAN_ID, TORAG_ID, DHAROK_ID,
-					AHRIM_ID, AKRISAE_ID, KARIL_ID);
+			ItemDefinitionLoader itemLoader = new ItemDefinitionLoader(cache);
+			//itemSearch(itemLoader, "seed");
+			
+			ItemDefinition id = itemLoader.load(32625);
+			//System.out.println(id.toString());
+			
+			QuestDefinitionLoader questLoader= new QuestDefinitionLoader(cache);
+			QuestDefinition quest = questLoader.load(175);
+			//System.out.println(quest);
+			
+			//questSearch(questLoader, "fairy");
+			//System.out.println(questLoader.load(154));
+			//System.out.println(questLoader.load(175));
+			
+			//System.out.println(quest.scriptId[0]);
+			new QuestDump(questLoader, true);
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -39,4 +61,59 @@ public class Main {
 		}
 	}
 
+	static ArrayList<Integer> itemSearch(ItemDefinitionLoader loader,
+			String... names) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		for (String s : names) {
+			s.toLowerCase();
+		}
+		int i = 32400;
+		while (true) {
+			String s = loader.load(i).name;
+			if(1>100000)break;
+			try {
+				for (String n : names) {
+					if (s.toLowerCase().contains(n)) {
+						res.add(i);
+						System.out.println(i + " : " + s);
+					}else{
+						//System.out.println(i + " : " + s);
+					}
+				}
+			} catch (Exception e) {
+
+			}
+			i++;
+		}
+		return res;
+	}
+	
+	
+	static ArrayList<Integer> questSearch(QuestDefinitionLoader loader,
+			String... names) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		for (String s : names) {
+			s.toLowerCase();
+		}
+		int i = 0;
+		while (true) {
+			String s = loader.load(i).name;
+			if(1>100000)break;
+			try {
+				for (String n : names) {
+					if (s.toLowerCase().contains(n)) {
+						res.add(i);
+						System.out.println(i + " : " + s);
+					}else{
+						//System.out.println(i + " : " + s);
+					}
+				}
+			} catch (Exception e) {
+
+			}
+			i++;
+		}
+		return res;
+	}
+	
 }
