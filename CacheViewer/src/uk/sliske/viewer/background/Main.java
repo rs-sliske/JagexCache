9cc85324bce16a1583142494e5d8b4ffe3e3c132
Robert <custard130@hotmail.com>
2015-05-03 12:43:08 +0100

    removed some redundant code

--- CacheViewer/src/uk/sliske/viewer/background/Main.java
+++ CacheViewer/src/uk/sliske/viewer/background/Main.java
@@ -1,8 +1,6 @@
 package uk.sliske.viewer.background;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 import uk.sliske.viewer.frame.CacheWindow;
 
@@ -21,49 +19,11 @@ public class Main {
 		}		
 		new Search(cache);
 		new CacheWindow();
+				
 	}
 
 	public static void main(String[] args) {
 		new Main();
-	}
-
-	 int[] toIntArray(List<Integer> args) {
-		int[] res = new int[args.size()];
-		for (int i = 0; i < args.size(); i++) {
-			res[i] = args.get(i);
-		}
-		return res;
-	}
-
-	 int[] toIntArray(String... args) {
-		ArrayList<Integer> i = new ArrayList<>();
-		for (String s : args) {
-			try {
-				int id = Integer.valueOf(s);
-				i.add(id);
-			} catch (Exception e) {
-
-			}
-		}
-		return toIntArray(i);
-	}
-
-	 boolean check(ArrayList<Integer> res, String s, String[] names) {
-		try {
-			for (String n : names) {
-				if (n.startsWith("*")) {
-					if (s.toLowerCase().equals(n.substring(1))) {
-						return true;
-					}
-				} else
-					if (s.toLowerCase().contains(n)) {
-						return true;
-					}
-			}
-		} catch (Exception e) {
-
-		}
-		return false;
 	}
 
 }
